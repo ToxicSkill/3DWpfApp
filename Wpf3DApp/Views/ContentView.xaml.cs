3b86911a6b150c4e12d2ba1394c5ee586bb1ad7e
Adam <61752657+ToxicSkill@users.noreply.github.com>
2023-08-15 13:20:21 +0200

    Add - Working example

--- Wpf3DApp/Views/ContentView.xaml.cs
+++ Wpf3DApp/Views/ContentView.xaml.cs
@@ -1,5 +1,11 @@
 ﻿using Wpf3DApp.ViewModels;
 using Wpf.Ui.Common.Interfaces;
+using System.Windows.Media.Media3D;
+using System;
+using System.Windows;
+using System.Drawing;
+using Point = System.Windows.Point;
+using System.Windows.Media;
 
 namespace Wpf3DApp.Views
 {
@@ -18,6 +24,62 @@ namespace Wpf3DApp.Views
             ViewModel = viewModel;
             InitializeComponent();
             DataContext = ViewModel;
+            globeModel.Geometry = GenerateSphere(new Point3D(), 1, 360, 180); 
+        } 
+        MeshGeometry3D GenerateSphere(Point3D center, double radius,
+                                    int slices, int stacks)
+        {
+            // Create the MeshGeometry3D.
+            MeshGeometry3D mesh = new MeshGeometry3D();
+
+            // Fill the Position, Normals, and TextureCoordinates collections.
+            for (int stack = 0; stack <= stacks; stack++)
+            {
+                double phi = Math.PI / 2 - stack * Math.PI / stacks;
+
+                for (int slice = 0; slice <= slices; slice++)
+                {
+                    double theta = slice * 2 * Math.PI / slices;
+
+                    Vector3D normal = (Vector3D)SphericalToCartesian(phi, theta, radius);
+                    mesh.Normals.Add(normal);
+                    mesh.Positions.Add(normal + center);
+                    mesh.TextureCoordinates.Add(
+                            new Point((double)slice / slices,
+                                      (double)stack / stacks));
+                }
+            }
+
+            // Fill the TriangleIndices collection.
+            for (int stack = 0; stack < stacks; stack++)
+                for (int slice = 0; slice < slices; slice++)
+                {
+                    int n = slices + 1; // Keep the line length down.
+
+                    if (stack != 0)
+                    {
+                        mesh.TriangleIndices.Add((stack + 0) * n + slice);
+                        mesh.TriangleIndices.Add((stack + 1) * n + slice);
+                        mesh.TriangleIndices.Add((stack + 0) * n + slice + 1);
+                    }
+                    if (stack != stacks - 1)
+                    {
+                        mesh.TriangleIndices.Add((stack + 0) * n + slice + 1);
+                        mesh.TriangleIndices.Add((stack + 1) * n + slice);
+                        mesh.TriangleIndices.Add((stack + 1) * n + slice + 1);
+                    }
+                }
+            return mesh;
+        }
+
+        Point3D SphericalToCartesian(double phi, double theta, double radius)
+        {
+            double y = radius * Math.Sin(phi);
+            double scale = -radius * Math.Cos(phi);
+            double x = scale * Math.Sin(theta);
+            double z = scale * Math.Cos(theta);
+
+            return new Point3D(x, y, z);
         }
     }
 }
